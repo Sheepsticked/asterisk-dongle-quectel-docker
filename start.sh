8eca311b28c81e20f691dff61dced8851b628da3
Sheepsticked <3026060@gmail.com>
2025-09-25 14:47:31 +0300

    changed to more appropriate permissions for /dev/snd

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Fix sound device permissions for quectel devices with UAC
-chown asterisk:asterisk -R /dev/snd/
+chown root:dialout -R /dev/snd/
 
 # Start Asterisk
 exec /usr/sbin/asterisk -f -n -Uasterisk -Gdialout "$@"
